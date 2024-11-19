29126b6ed5a1f2a9bc557270f9390ec021082a93
artemchik9321 <artemtatsin9321@gmail.com>
2024-11-19 20:59:01 +0200

    Add project files.

---
+++ OOPSHABLN.cpp
@@ -0,0 +1,12 @@
+// OOPSHABLN.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "Library.h"
+
+int main()
+{
+	Point<int>obj1(10);
+	obj1.Print();
+	obj1.Init(15, 20);
+	obj1.Print();
+}
